789393e329f31b43b46f12209f5c2a8968e959c9
Max Fisher <maxmfishernj@gmail.com>
2021-06-30 08:34:46 -0400

    removed complications

--- cordovaProxy.swift
+++ cordovaProxy.swift
@@ -1,3 +1,10 @@
+//
+//  SwLoader.swift
+//  HelloCordova
+//
+//  Created by Max Fisher on 6/28/21.
+//
+
 import Foundation
 import WebKit
 
@@ -13,36 +20,38 @@ import WebKit
         guard let path = urlSchemeTask.request.url?.path else {
             return false
         }
-        if path.hasPrefix("/_cordova_proxy_") {
-            let webViewTarget = path.replacingOccurrences(of: "/_cordova_proxy_/", with: "")
-            let base = Bundle.main.bundleURL.appendingPathComponent("www")
-            let deviceLocation = base.appendingPathComponent(webViewTarget)
-            let task = URLSession.shared.dataTask(with: deviceLocation) { [self] (data,response,error) in
-                if stoppedTasks.contains(urlSchemeTask) {
-                    print("Url Scheme tasked stoped while request was in flight")
-                } else if error != nil {
-                    urlSchemeTask.didFailWithError(error!)
-                } else {
-                    let headers = [
-                        "Access-Control-Allow-Origin":"*",
-                        "Cache-Control": "no-cache"
-                    ]
-                    let fake_url = URL(string: "http://cordova-proxy/".appending(webViewTarget))!
-                    let fake_res = HTTPURLResponse(url: fake_url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: headers)
-                    urlSchemeTask.didReceive(fake_res!)
-                    urlSchemeTask.didReceive(data!)
-                    urlSchemeTask.didFinish()
-                }
-            }
-            task.resume()
-
-        } else {
+        if !path.hasPrefix("/_cordova_proxy_/") {
             return false
         }
+        let webViewTarget = path.replacingOccurrences(of: "/_cordova_proxy_/", with: "")
+        let base = Bundle.main.bundleURL.appendingPathComponent("www")
+        let deviceLocation = base.appendingPathComponent(webViewTarget)
+        let task = URLSession.shared.dataTask(with: deviceLocation) { [self] (data,response,error) in
+            if stoppedTasks.contains(urlSchemeTask) {
+                print("Url Scheme tasked stoped while request was in flight")
+            } else if error != nil {
+                urlSchemeTask.didFailWithError(error!)
+            } else {
+                let headers = [
+                    "Access-Control-Allow-Origin":"*",
+                    "Cache-Control": "no-cache"
+                ]
+                let fake_url = URL(string: "http://cordova-proxy/".appending(webViewTarget))!
+                let fake_res = HTTPURLResponse(url: fake_url, statusCode: 200, httpVersion: "HTTP/1.1", headerFields: headers)
+                urlSchemeTask.didReceive(fake_res!)
+                urlSchemeTask.didReceive(data!)
+                urlSchemeTask.didFinish()
+            }
+        }
+        task.resume()
         return true
     }
     
-    @objc func stopSchemeTask(_ schemeTask:WKURLSchemeTask) {
-        stoppedTasks.add(schemeTask)
+    @objc func stopSchemeTask(_ urlSchemeTask:WKURLSchemeTask) {
+        stoppedTasks.add(urlSchemeTask)
+    }
+    
+    @objc func log(_ command:CDVInvokedUrlCommand) {
+        print(command.arguments ?? "Could Not log arguments ")
     }
 }
